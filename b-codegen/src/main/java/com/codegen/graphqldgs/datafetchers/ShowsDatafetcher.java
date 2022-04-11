097f9826a6c4ab1af232edc1be09b6e792fc7444
kuanghc1 <kuanghc1@lenovo.com>
2022-04-11 17:45:03 +0800

    Add constant

--- b-codegen/src/main/java/com/codegen/graphqldgs/datafetchers/ShowsDatafetcher.java
+++ b-codegen/src/main/java/com/codegen/graphqldgs/datafetchers/ShowsDatafetcher.java
@@ -1,9 +1,10 @@
 package com.codegen.graphqldgs.datafetchers;
 
+import com.codegen.graphqldgs.DgsConstants;
 import com.codegen.graphqldgs.services.ShowsService;
 import com.codegen.graphqldgs.types.Show;
 import com.netflix.graphql.dgs.DgsComponent;
-import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.InputArgument;
 
 import java.util.List;
@@ -21,7 +22,7 @@ public class ShowsDatafetcher {
      * This datafetcher resolves the shows field on Query.
      * It uses an @InputArgument to get the titleFilter from the Query if one is defined.
      */
-    @DgsQuery
+    @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.Shows)
     public List<Show> shows(@InputArgument("titleFilter") String titleFilter) {
         if (titleFilter == null) {
             return showsService.shows();
