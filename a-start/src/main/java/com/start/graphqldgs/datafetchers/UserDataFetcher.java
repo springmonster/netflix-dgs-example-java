3b0f5177e5b8d4bef9b59e5c60f9ba931c3a6698
kuanghc1 <kuanghc1@lenovo.com>
2022-04-12 15:39:11 +0800

    Add Mutation

--- a-start/src/main/java/com/start/graphqldgs/datafetchers/UserDataFetcher.java
+++ a-start/src/main/java/com/start/graphqldgs/datafetchers/UserDataFetcher.java
@@ -1,6 +1,7 @@
 package com.start.graphqldgs.datafetchers;
 
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.start.graphqldgs.types.User;
 
@@ -9,5 +10,10 @@ public class UserDataFetcher {
     @DgsQuery
     public User user() {
         return new User("id1", "zhangsan");
+    }
+
+    @DgsMutation
+    public User addUser(String id, String name) {
+        return new User(id, name);
     }
 }
