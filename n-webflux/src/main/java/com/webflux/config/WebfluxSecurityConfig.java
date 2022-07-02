31747c51f35351db42261c5b9eb029d5d5b161e9
kuanghc1 <kuanghc1@lenovo.com>
2022-07-02 22:23:25 +0800

    Add webflux security but seems not support

---
+++ n-webflux/src/main/java/com/webflux/config/WebfluxSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.webflux.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebfluxSecurityConfig {
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
---
+++ n-webflux/src/main/java/com/webflux/datafetcher/SecureDataFetcher.java
@@ -0,0 +1,28 @@
+package com.webflux.datafetcher;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import reactor.core.publisher.Mono;
+
+@DgsComponent
+public class SecureDataFetcher {
+
+    @DgsQuery
+    public Mono<String> secureNone() {
+        return Mono.just("hello everyone!");
+    }
+
+    @DgsQuery
+    @PreAuthorize("hasRole('USER')")
+    public Mono<String> secureUser() {
+        return Mono.just("hello user or admin!");
+    }
+
+    @DgsQuery
+    @Secured("{ROLE_ADMIN}")
+    public Mono<String> secureAdmin() {
+        return Mono.just("hello admin!");
+    }
+}
